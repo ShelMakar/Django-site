14e8f65c329a09f2028b56991037bf8e00394765
Макар Шеляг <makarshelyag@yandex.ru>
2024-10-10 00:21:20 +0300

    1

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -46,10 +46,10 @@ MIDDLEWARE = [
 ]
 
 if DEBUG:
-    MIDDLEWARE += ("debug_toolbar.middleware.DebugToolbarMiddleware",)
-    INSTALLED_APPS += ("debug_toolbar",)
+    MIDDLEWARE += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+    INSTALLED_APPS += ('debug_toolbar',)
     INTERNAL_IPS = [
-        "127.0.0.1",
+        '127.0.0.1',
     ]
 
 ROOT_URLCONF = 'lyceum.urls'
