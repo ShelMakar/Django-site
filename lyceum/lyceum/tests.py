afd9ffe3ecd374c389dd325c186c2ef9120338b2
Макар Шеляг <makarshelyag@yandex.ru>
2024-10-11 19:43:35 +0300

    исправленное задание 3

--- lyceum/lyceum/tests.py
+++ lyceum/lyceum/tests.py
@@ -1,4 +1,5 @@
-from django.test import Client, override_settings, TestCase
+from django.test import Client, TestCase
+from django.test import override_settings
 
 
 from lyceum.middleware import Middleware
