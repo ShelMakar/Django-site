4adcf2fd3f0351d8c577f587cad2fc67410998ef
Макар Шеляг <makarshelyag@yandex.ru>
2024-10-11 21:15:58 +0300

    issues

--- README.md
+++ README.md
@@ -1,25 +1,33 @@
-## статус пайплайна
+## ПРОЕКТ ЯНДЕКС СПЕЦИАЛИЗАЦИИ
 ![pipeline status](https://gitlab.crja72.ru/django/2024/autumn/course/students/172544-makarshelyag-course-1187/badges/main/pipeline.svg)
 
-## ативация виртуального окружения
+## На странице обзора проекта в правом верхнем углу выберите 'Код', затем скопируйте URL для 'клонирования с использованием HTTPS.'
+## Откройте терминал и перейдите в каталог, в который вы хотите клонировать файлы.
+## Выполните следующую команду. Git автоматически создаст папку с именем репозитория и загрузит в неё файлы.
+```bash
+git clone <copied URL>
+``` 
+
+## Для корректной работы скачайте Python версии 3.12
+## Активация виртуального окружения
 ``` bash
 python3 -m venv venv
 source venv/bin/activate
 ```
 
-## установка зависимостей из папки 'requirements'
+## Установка зависимостей из папки 'requirements'
 ``` bash
 pip install -r prod.txt
 pip install -r dev.txt
 pip install -r test.txt
 ```
 
-## для корретного запуска скопируйте файл config.env
+## Для корректного запуска скопируйте файл config.env
 ``` bash
 cp config.env .env
 ```
 
-## запуск
+## Запуск
 ``` bash
 cd lyceum
 python3 manage.py runserver
--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -5,11 +5,7 @@ import dotenv
 
 dotenv.load_dotenv()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'base_key')
@@ -33,7 +29,6 @@ ALLOW_REVERSE = ALLOW_REVERSE_ENV in (
 )
 
 
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -42,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'catalog.apps.CatalogConfig',
     'about.apps.AboutConfig',
+    'catalog.apps.CatalogConfig',
     'homepage.apps.HomepageConfig',
 ]
 
@@ -85,8 +80,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'lyceum.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -95,8 +88,6 @@ DATABASES = {
     }
 }
 
-# Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -124,8 +115,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Internationalization
-# https://docs.djangoproject.com/en/5.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -135,12 +124,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
