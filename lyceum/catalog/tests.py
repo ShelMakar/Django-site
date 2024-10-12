be790c05c3ebb6d8750bd2b22b573a85bc5b8afb
Макар Шеляг <makarshelyag@yandex.ru>
2024-10-12 23:23:01 +0300

    issue test

--- lyceum/catalog/tests.py
+++ lyceum/catalog/tests.py
@@ -1,44 +1,17 @@
 import http
 
+from parametrize import parametrize
 import django.test
 
 
 class NumbersTest(django.test.TestCase):
 
-    def test_catalog(self):
-        response = django.test.Client().get('/catalog/')
-        self.assertEqual(response.status_code, http.HTTPStatus.OK, 'ошибка')
+    @parametrize('url', ['/catalog/', '/catalog/1/', '/catalog/re/4548878/'])
+    def test_poz(self, url):
+        response = django.test.Client().get(url)
+        self.assertEqual(response.status_code, http.HTTPStatus.OK, 'ошибка в тесте на ответ 200')
 
-    def test_catalog_int(self):
-        response = django.test.Client().get('/catalog/1/')
-        self.assertEqual(response.status_code, http.HTTPStatus.OK, 'нельзя не число')
-
-    def test_catalog_not_int(self):
-        response = django.test.Client().get('/catalog/asasasasasa/')
-        self.assertEqual(response.status_code, http.HTTPStatus.NOT_FOUND, 'за строки карают')
-
-    def test_return_string(self):
-        response = django.test.Client().get('/catalog/re/asasasasasa/')
-        self.assertEqual(response.status_code, http.HTTPStatus.NOT_FOUND, 'за строки карают')
-
-    def test_return_not_int(self):
-        response = django.test.Client().get('/catalog/re/-45454/')
-        self.assertEqual(response.status_code, http.HTTPStatus.NOT_FOUND)
-
-    def test_return_int(self):
-        response = django.test.Client().get('/catalog/re/4548878/')
-        self.assertEqual(response.status_code, http.HTTPStatus.OK)
-        self.assertContains(response, int(4548878))
-
-    def test_converter_string(self):
-        response = django.test.Client().get('/catalog/re/asasasasasa/')
-        self.assertEqual(response.status_code, http.HTTPStatus.NOT_FOUND, 'за строки карают')
-
-    def test_converter_not_int(self):
-        response = django.test.Client().get('/catalog/re/-45454/')
-        self.assertEqual(response.status_code, http.HTTPStatus.NOT_FOUND)
-
-    def test_converter_int(self):
-        response = django.test.Client().get('/catalog/re/4548878/')
-        self.assertEqual(response.status_code, http.HTTPStatus.OK)
-        self.assertContains(response, int(4548878))
+    @parametrize('url', ['/catalog/asasasasasa/', '/catalog/re/asasasasasa/', '/catalog/re/-45454/'])
+    def test_neg(self, url):
+        response = django.test.Client().get(url)
+        self.assertEqual(response.status_code, http.HTTPStatus.NOT_FOUND, 'ошибка в тесте на ответ 404')
