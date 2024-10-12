27866e20badd8f987ce1868ea5287b9317389b3c
Макар Шеляг <makarshelyag@yandex.ru>
2024-10-12 23:23:40 +0300

    issue test

--- lyceum/catalog/tests.py
+++ lyceum/catalog/tests.py
@@ -9,9 +9,24 @@ class NumbersTest(django.test.TestCase):
     @parametrize('url', ['/catalog/', '/catalog/1/', '/catalog/re/4548878/'])
     def test_poz(self, url):
         response = django.test.Client().get(url)
-        self.assertEqual(response.status_code, http.HTTPStatus.OK, 'ошибка в тесте на ответ 200')
+        self.assertEqual(
+            response.status_code,
+            http.HTTPStatus.OK,
+            'ошибка в тесте на ответ 200',
+        )
 
-    @parametrize('url', ['/catalog/asasasasasa/', '/catalog/re/asasasasasa/', '/catalog/re/-45454/'])
+    @parametrize(
+        'url',
+        [
+            '/catalog/asasasasasa/',
+            '/catalog/re/asasasasasa/',
+            '/catalog/re/-45454/',
+        ],
+    )
     def test_neg(self, url):
         response = django.test.Client().get(url)
-        self.assertEqual(response.status_code, http.HTTPStatus.NOT_FOUND, 'ошибка в тесте на ответ 404')
+        self.assertEqual(
+            response.status_code,
+            http.HTTPStatus.NOT_FOUND,
+            'ошибка в тесте на ответ 404',
+        )
