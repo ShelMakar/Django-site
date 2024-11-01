3993aa7ff6516c29bef314e63dd7279300c0f36d
Макар Шеляг <makarshelyag@yandex.ru>
2024-11-01 17:25:15 +0300

    3 задание лекция 5

--- lyceum/download/views.py
+++ lyceum/download/views.py
@@ -5,13 +5,15 @@ import django.http
 
 
 def download_image(request, path):
-    path = pathlib.Path(django.conf.settings.MEDIA_ROOT) / path
-    path.exists()
-    return django.http.FileResponse(
-        open(path, 'rb'),
+    file_path = pathlib.Path(django.conf.settings.MEDIA_ROOT) / path
+    file_path.exists()
+    response = django.http.FileResponse(
+        open(file_path, 'rb'),
         as_attachment=True,
         content_type='image',
     )
+    response['Content-Disposition'] = 'attachment; filename="%s"' % file_path
+    return response
 
 
 __all__ = ['download_image']
